daec97809397492f68df8e2ca4a34ae9625a6d4e
Michael Kim II <mjkim21@asu.edu>
2026-09-10 12:32:31 -0700

    Modify link and demo fields in projects.js
    Updated the link and demo fields for the Honeywell Aerospace internship entry.

--- projects.js
+++ projects.js
@@ -21,8 +21,8 @@ const PROJECTS = [
     name: "Honeywell Aerospace Radiation Effects Internship",
     description: "This internship gave me experience in Radiation Effects from a Systems Engineering perspective, with responsibilities spanning the design and testing of semiconductor devices for TID, FXR, and neutron-induced displacement damage (nDD) effects.",
     tech: ["KiCAD", "Microsoft Office Suite", "Creme96", "OMERE"],
-    link: "Although most of the stuff is Honeywell Aerospace proprietary information, I created a slide deck for my lab showing what I learned. The PDF of the slides can be found [here](content/radiationpresentation.pdf). ",
-    //demo: "n/a",
+    link: "",
+    demo: "n/a",
     year: "2026"
   },
   {
