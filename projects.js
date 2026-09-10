2b012f8724c6b5e99b5cc9a765005dfc13ede63a
Michael Kim II <mjkim21@asu.edu>
2026-09-10 12:46:30 -0700

    Update link for Honeywell Aerospace internship

--- projects.js
+++ projects.js
@@ -21,7 +21,7 @@ const PROJECTS = [
     name: "Honeywell Aerospace Radiation Effects Internship",
     description: "This internship gave me experience in Radiation Effects from a Systems Engineering perspective, with responsibilities spanning the design and testing of semiconductor devices for TID, FXR, and neutron-induced displacement damage (nDD) effects.",
     tech: ["KiCAD", "Microsoft Office Suite", "Creme96", "OMERE"],
-    link: "https://github.com/mjkim21-dev/content/radiationpresentation.pdf",
+    link: "https://github.com/mjkim21-dev/portfolio/blob/af4b4e60aaaee8b557e74d130c834a15e3ecfd1c/content/raditationpresentation.pdf",
     //demo: "n/a",
     year: "2026"
   },
