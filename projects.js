18ae94974ce243aca590816612da2f44a58f1e08
Michael Kim II <mjkim21@asu.edu>
2026-09-10 16:55:20 -0700

    Correct spelling of 'KiCAD' to 'KiCad'

--- projects.js
+++ projects.js
@@ -28,7 +28,7 @@ const PROJECTS = [
   {
     name: "Honeywell Aerospace Radiation Effects Internship",
     description: "This internship gave me experience in Radiation Effects from a Systems Engineering perspective, with responsibilities spanning the design of test plans for semiconductor devices for TID, FXR, and neutron-induced displacement damage (nDD) effects. I have put together some slides showing how radiation affects electronic devices. *For more info, the PDF will be downloaded when you click the link button.*",
-    tech: ["KiCAD", "Microsoft Office Suite", "Creme96", "OMERE"],
+    tech: ["KiCad", "Microsoft Office Suite", "Creme96", "OMERE"],
     link: "https://github.com/mjkim21-dev/portfolio/raw/main/content/raditationpresentation.pdf",
     //demo: "n/a",
     year: "Summer 2026"
