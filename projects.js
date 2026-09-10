b23f91765dc54576c7894da8a788f1bbd658c82d
Michael Kim II <mjkim21@asu.edu>
2026-09-10 15:31:37 -0700

    Refine internship description for clarity
    Updated the description of the Honeywell Aerospace internship to clarify responsibilities regarding test plans.

--- projects.js
+++ projects.js
@@ -27,7 +27,7 @@ const PROJECTS = [
   },
   {
     name: "Honeywell Aerospace Radiation Effects Internship",
-    description: "This internship gave me experience in Radiation Effects from a Systems Engineering perspective, with responsibilities spanning the design and testing of semiconductor devices for TID, FXR, and neutron-induced displacement damage (nDD) effects.",
+    description: "This internship gave me experience in Radiation Effects from a Systems Engineering perspective, with responsibilities spanning the design of test plans of semiconductor devices for TID, FXR, and neutron-induced displacement damage (nDD) effects.",
     tech: ["KiCAD", "Microsoft Office Suite", "Creme96", "OMERE"],
     link: "https://github.com/mjkim21-dev/portfolio/raw/main/content/raditationpresentation.pdf",
     //demo: "n/a",
